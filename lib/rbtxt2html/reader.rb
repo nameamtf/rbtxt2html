5713213b8295c0fafb09a664ed446f4dbe18f921
Edvaldo Silva de Almeida Júnior <edvaldoajunior@gmail.com>
2016-01-30 07:05:01 -0200

    Tests for class Writer improved

--- lib/rbtxt2html/reader.rb
+++ lib/rbtxt2html/reader.rb
@@ -1,26 +1,11 @@
 module RbTxt2HTML
     
-  class Reader
+  class Reader < RbTxt2HTML::Base
     
     def initialize(fname)
       if is_string?(fname) and is_file?(fname) and has_contents?(fname)
         @filename = fname
       end
-    end
-    
-    def is_string?(fname)
-      raise ArgumentError.new('Invalid file name.') unless fname.is_a? String
-      true
-    end
-      
-    def is_file?(fname)
-      raise IOError.new('File not found.') unless File.exist?(fname)
-      true
-    end
-    
-    def has_contents?(fname)
-      raise IOError.new('File empty.') if File.zero?(fname)
-      true
     end
     
     def read_file
