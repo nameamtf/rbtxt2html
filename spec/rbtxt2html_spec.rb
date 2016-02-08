f63c2ae727cfbf413dcbf9d955070c43d51bfa65
Edvaldo Silva de Almeida Júnior <edvaldoajunior@gmail.com>
2016-02-08 14:51:15 -0200

    Changing the rspec tests for the new scenario

--- spec/rbtxt2html_spec.rb
+++ spec/rbtxt2html_spec.rb
@@ -42,18 +42,18 @@ describe RbTxt2HTML do
     
     it 'should raise an IOError (File not found) if initialization parameter is not a valid file' do
       expect {
-        c = RbTxt2HTML::Reader.new('/home/edvaldo/teste2.txt')
+        c = RbTxt2HTML::Reader.new('teste2.txt')
       }.to raise_error(IOError, 'File not found.') 
     end
     
     it 'should raise an IOError (File empty) if the file is empty' do
       expect {
-        c = RbTxt2HTML::Reader.new('/home/edvaldo/teste.txt')
+        c = RbTxt2HTML::Reader.new('teste.txt')
       }.to raise_error(IOError,'File empty.')
     end
     
     it 'should read the file' do
-      c = RbTxt2HTML::Reader.new('/home/edvaldo/texto.txt')
+      c = RbTxt2HTML::Reader.new('texto.txt')
       v = c.read_file
       expect(v).to eq(['Linha sem qualquer tag.','%T1% Título 1','%T2% Título 2','%T3% Título 3','%T4% Título 4','%T5% Título 5','%T6% Título 6'])
     end
@@ -74,27 +74,27 @@ describe RbTxt2HTML do
     
     it 'should raise an IOError (File already exists) if initialization parameter fname is the name of an existing file' do
       expect {
-        c = RbTxt2HTML::Writer.new('/home/edvaldo/texto.txt',[])
+        c = RbTxt2HTML::Writer.new('texto.txt',[])
       }.to raise_error(IOError, 'File already exists.') 
     end  
     
     it 'should raise an ArgumentError (Invalid file name) if initialization parameter lines is not an Array' do
       expect {
-        c = RbTxt2HTML::Writer.new('/home/edvaldo/texto2.txt',2)
+        c = RbTxt2HTML::Writer.new('texto2.txt',2)
       }.to raise_error(ArgumentError, 'Parameter is NOT an array.')
     end   
     
     it 'should raise an ArgumentError (Invalid file name) if initialization parameter lines is an empty Array' do
       expect {
-        c = RbTxt2HTML::Writer.new('/home/edvaldo/texto2.txt',[])
+        c = RbTxt2HTML::Writer.new('texto2.txt',[])
       }.to raise_error(ArgumentError, 'Lines array is empty.')
     end
     
     it 'should write the file' do
       File.delete('/home/edvaldo/texto.txt.html')
-      c = RbTxt2HTML::Writer.new('/home/edvaldo/texto.txt.html',['<p>Linha sem qualquer tag.</p>','<h1>Título 1</h1>','<h2>Título 2</h2>','<h3>Título 3</h3>','<h4>Título 4</h4>','<h5>Título 5</h5>','<h6>Título 6</h6>'])
+      c = RbTxt2HTML::Writer.new('texto.txt.html',['<p>Linha sem qualquer tag.</p>','<h1>Título 1</h1>','<h2>Título 2</h2>','<h3>Título 3</h3>','<h4>Título 4</h4>','<h5>Título 5</h5>','<h6>Título 6</h6>'])
       c.write_file
-      ff = File.exist?('/home/edvaldo/texto.txt.html')
+      ff = File.exist?('texto.txt.html')
       expect(ff).to be true
     end
     
